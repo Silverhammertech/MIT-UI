3efddabe2313dda4749f895ece3df9448075150f
Steven Gregory <steven@silverhammertech.com>
2018-08-30 21:04:31 -0500

    Stop CultivationPage crash and removed app.css

--- canebiz-app/src/containers/CultivatorFacilityPage/CultivatorFacilityPage.js
+++ canebiz-app/src/containers/CultivatorFacilityPage/CultivatorFacilityPage.js
@@ -3,8 +3,6 @@ import React, { Component } from 'react';
 
 import Layout from '../../components/Layout/Layout';
 import LabStatusTile from '../../components/Cultivator/LabStatus/LabStatusTile';
-import WeedProductCard from '../../components/Cultivator/WeedProductCard/WeedProductCard';
-import WeedProductTable from '../../components/Cultivator/WeedProductTable/WeedProductTable';
 
 
 class CultivatorFacilityPage extends Component {
@@ -15,16 +13,8 @@ class CultivatorFacilityPage extends Component {
                 <Layout name="Cultivator" >
                 
                 <div>
-                    <WeedProductCard id="Lab1" temperature="74" />
+                    <LabStatusTile id="Lab1" temperature="74" />
                 </div>
-                <div>
-                    <WeedProductCard id id="Lab2" temperature="71" />
-                </div>
-            
-                <div>
-                    <WeedProductTable />
-                </div>
-            
             
 
               </Layout>

f45a927e81ffdb3e8318ad71369c663f87a4799a
Steven Gregory <steven@silverhammertech.com>
2018-08-30 20:40:12 -0500

    Improving content

--- canebiz-app/src/components/Cultivator/WeedProductCard/WeedProductCard.js
+++ canebiz-app/src/components/Cultivator/WeedProductCard/WeedProductCard.js
@@ -16,11 +16,11 @@ class WeedProductCard extends Component {
 
         <CardImg top width="100%" src="../../../../public/assets/images/white-rhino.jpg" alt="Card image cap" />
         <CardBody>
-          <CardTitle>{this.props.plant.city}</CardTitle>
+          <CardTitle>{this.props.plant.name}</CardTitle>
           <CardSubtitle>{this.props.plant.city}</CardSubtitle>
-          <CardText>{this.props.plant.city}</CardText>
-          <CardText>{this.props.plant.city}</CardText>
-          <CardText>{this.props.plant.city}</CardText>
+          <CardText>{this.props.plant.address}</CardText>
+          <CardText>{this.props.plant.intro_body}</CardText>
+
           <Button>Add to cart</Button>
         </CardBody>
       </Card>

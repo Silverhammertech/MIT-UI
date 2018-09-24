1021b1e5b90e15fb3121c942520c0fe42c5d8af2
Steven Gregory <steven@silverhammertech.com>
2018-09-24 15:59:08 -0500

    Checkout Added and NavBar Reorg

--- canebiz-app/src/App.js
+++ canebiz-app/src/App.js
@@ -3,7 +3,6 @@ import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import { BrowserRouter } from 'react-router-dom';
 import CanebizNexus from './containers/CanebizNexus/CanebizNexus';
-import CultivatorFacilityPage from './containers/CultivatorFacilityPage/CultivatorFacilityPage';
 import CultivatorDashboard from './containers/CultivatorFacilityPage/CultivatorDashboard';
 import ManufacturerFacilityPage from './containers/ManufacturerFacilityPage/ManufacturerFacilityPage';
 import DistributionFacilityPage from './containers/DistributionFacilityPage/DistributionFacilityPage';
@@ -14,6 +13,8 @@ import Aux from './hoc/_Aux/_Aux';
 import Layout from './components/Layout/Layout';
 import Blog from './components/Blog/Blog';
 import CanaChat from './containers/CanaChat/CanaChat';
+import Checkout from './containers/Checkout/Checkout';
+import PlantSearch from './containers/Search/PlantSearch';
 
 class App extends Component {
   render () {
@@ -26,16 +27,17 @@ class App extends Component {
                         <Switch>
                             {/* /nexus is used as base screen, as Root / is App and there is no screen associated */}
                             
+                            <Route path="/caneblog" exact component={Blog} />
                             <Route path="/users" exact component={UserPage} />
-                            <Route path="/cultivators" exact component={CultivatorFacilityPage} />
                             <Route path="/cultivatorsdash" exact component={CultivatorDashboard} />
                             <Route path="/manufacturers" exact component={ManufacturerFacilityPage} />
                             <Route path="/distributors" exact component={DistributionFacilityPage} />
                             <Route path="/dispensariess" exact component={DispensaryFacilityPage} />
+                            <Route path="/plantsearch" exact component={PlantSearch} />
                             <Route path="/canebizNexus" exact component={CanebizNexus} />
                             <Route path="/plantcatalog" exact component={PlantCatalog} />
-                            <Route path="/caneblog" exact component={Blog} />
                             <Route path="/canechat" exact component={CanaChat} />
+                            <Route path="/checkout" exact component={Checkout} />
                        </Switch>
 
                 </div>
--- canebiz-app/src/components/Navigation/NavigationItems/NavigationItems.js
+++ canebiz-app/src/components/Navigation/NavigationItems/NavigationItems.js
@@ -6,16 +6,17 @@ import NavigationItem from './NavigationItem/NavigationItem';
 const navigationItems = () => (
     <ul className={classes.NavigationItems}>
         <NavigationItem link="/" exact>Home</NavigationItem>
+        <NavigationItem link="/CanebizNexus">CanebizNexus</NavigationItem>
         <NavigationItem link="/users">User</NavigationItem>
-        <NavigationItem link="/cultivators">Cultivator</NavigationItem>
         <NavigationItem link="/cultivatorsdash">CultivatorDash</NavigationItem>
         <NavigationItem link="/manufacturers">Manufacturer</NavigationItem>
         <NavigationItem link="/distributors">Distributors</NavigationItem>
         <NavigationItem link="/dispensaries">Dispensaries</NavigationItem>
-        <NavigationItem link="/CanebizNexus">CanebizNexus</NavigationItem>
+        <NavigationItem link="/plantsearch">PlantSearch</NavigationItem>
         <NavigationItem link="/PlantCatalog">PlantCatalog</NavigationItem>
-        <NavigationItem link="/caneblog">CaneBlog</NavigationItem>
         <NavigationItem link="/canechat">CanaChat</NavigationItem>
+        <NavigationItem link="/caneblog">CaneBlog</NavigationItem>
+        <NavigationItem link="/Checkout">Checkout</NavigationItem>
 
         
     </ul>
